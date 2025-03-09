1b8e30ffdf0d9bdfcba213dbc3ae2f2c30092e7d
adam6518 <getamped75@gmail.com>
2025-03-10 01:54:49 +0700

    login page done

---
+++ src/pages/DataProject.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class DataProject extends Component {
+  render() {
+    return (
+      <div>DataProject</div>
+    )
+  }
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "../assets/styles/Login.css";
+import logoMU from "../assets/img/logo-mu.jpg"
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="form-login">
+        <form className="border border-black">
+        <img clasName="form-img" src={logoMU} alt="logo-mu" />
+          <div className="form-username">
+            <label className="form-label">Username</label>
+            <input className="form-control" placeholder="Input username..." />
+          </div>
+          <div className="form-password">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Input password..."
+            />
+          </div>
+          <button type="submit" className="form-btn-sign-in btn btn-warning">
+            Sign In
+          </button>
+          <p className="form-p">Notes : Harap hubungi Admin jika belum mempunyai akun !</p>
+        </form>
+      </div>
+    );
+  }
+}
